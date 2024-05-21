d3e5dd79b22edbbf2a2a9793aa6075c26d950147
mdaamer248 <mdaamer248@gmail.com>
2024-05-21 13:39:22 +0530

    lessons added

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const mssql = require("mssql");
 const db = require("./Database/database");
 const router = express.Router();
 
-router.get("/get/syllabus/course/:courseId", async (req, res) => {
+router.get("/syllabus/course/:courseId", async (req, res) => {
   try {
     const courseId = req.params.courseId;
 
@@ -18,7 +18,6 @@ router.get("/get/syllabus/course/:courseId", async (req, res) => {
     if (result.recordset.length === 0) {
       res.status(404).json({ message: "No course found with this id." });
     } else {
-      console.log(result.recordset[0]);
       res.json(result.recordset[0]);
     }
   } catch (err) {
@@ -53,6 +52,36 @@ router.get("/get/all/lessons/:courseId", async (req, res) => {
     res
       .status(500)
       .json({ message: "Error retrieving lessons", error: err.message });
+  }
+});
+
+// READ: Get all syllabus Notes for a given course ID
+router.get("/syllabus/notes/:courseId", async (req, res) => {
+  try {
+    const courseId = req.params.courseId;
+
+    // Create a connection pool
+    const pool = await db.connect();
+
+    // Execute the query using the connection pool
+    const result = await pool
+      .request()
+      .input("courseId", mssql.Int, courseId)
+      .query("SELECT * FROM admin_syllabusNotes WHERE SylabusID = @courseId");
+
+    // Check if any rows were returned
+    if (result.recordset.length === 0) {
+      console.log("No notes found for this course id.");
+      res.status(404).json({ message: "No notes found for this course id." });
+    } else {
+      console.log(result.recordset);
+      res.json(result.recordset);
+    }
+  } catch (err) {
+    console.error("Error retrieving notes:", err);
+    res
+      .status(500)
+      .json({ message: "Error retrieving notes", error: err.message });
   }
 });
 
