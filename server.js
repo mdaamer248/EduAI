6b364972db3bedf8b31c877b6d5932fd82b70a34
mdaamer248 <mdaamer248@gmail.com>
2024-05-28 10:50:17 +0530

    cors added.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 // const db = require("./Database/database");
 
 db = require("./Database/database");
 const routes = require("./router");
 
 const app = express();
+
+app.use(cors());
 app.use("/api", routes);
 
 try {
@@ -16,4 +19,5 @@ try {
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
 module.exports = app;
